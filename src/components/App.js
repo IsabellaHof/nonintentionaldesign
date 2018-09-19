d3006434e2e4553911c2228cecec94c2de07ba5a
IsabellaHof <isabella.hof@web.de>
2018-09-19 10:55:28 +0200

    create screen folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import ImageScreen from './ImageScreen'
+import ImageScreen from './screen/ImageScreen'
 
 class App extends Component {
   render() {
--- src/components/screen/ImageScreen.js
+++ src/components/screen/ImageScreen.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-import nid_1 from '../img/nid_1.jpg'
-import nid_2 from '../img/nid_2.jpg'
-import nid_3 from '../img/nid_3.jpg'
-import nid_4 from '../img/nid_4.jpg'
-import nid_5 from '../img/nid_5.jpg'
-import nid_6 from '../img/nid_6.jpg'
-import nid_7 from '../img/nid_7.jpg'
-import nid_8 from '../img/nid_8.jpg'
-import ImageScreenHeader from './ImageScreenHeader.js'
+import nid_1 from '../../img/nid_1.jpg'
+import nid_2 from '../../img/nid_2.jpg'
+import nid_3 from '../../img/nid_3.jpg'
+import nid_4 from '../../img/nid_4.jpg'
+import nid_5 from '../../img/nid_5.jpg'
+import nid_6 from '../../img/nid_6.jpg'
+import nid_7 from '../../img/nid_7.jpg'
+import nid_8 from '../../img/nid_8.jpg'
+import ImageScreenHeader from '../ImageScreenHeader'
 
 const StyledImageContainer = styled.div`
   columns: 2;
