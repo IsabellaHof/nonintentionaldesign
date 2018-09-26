962a2dc31ed33cb04aa37a4f32b3fb888cac3e4d
IsabellaHof <isabella.hof@web.de>
2018-09-26 13:53:43 +0200

    add toggle function

--- client/src/components/screen/InfoScreen.js
+++ client/src/components/screen/InfoScreen.js
@@ -4,51 +4,153 @@ import { Link } from 'react-router-dom'
 import logo_big from '../../img/logo_big.svg'
 import arrow_right from '../../img/arrow_right.svg'
 
-const Container = styled.div`
+const InfoScreenContainer = styled.div`
+  align-content: center;
   display: flex;
-  align-content: center;
   justify-content: center;
-`
-const InfoScreenGrid = styled.div`
-  display: grid;
-  align-content: center;
-  justify-content: center;
-  columns: 1;
+  flex-direction: column;
 `
 const InfoScreenImage = styled.div`
-  margin-top: 80px;
   display: flex;
   justify-content: center;
+  margin-top: 80px;
 `
 const InfoScreenHeadline = styled.div`
-  margin-top: 60px;
   color: #fc4955;
   font-family: 'Roboto Mono';
-  font-style: italic;
   font-size: 48px;
+  font-style: italic;
   font-weight: 700;
+  margin-top: 60px;
 `
 const InfoScreenRunningText = styled.div`
-  margin-top: 10px;
   color: #000000;
   font-family: 'Loro';
   font-size: 38px;
   font-weight: 700;
+  margin-top: 10px;
+  margin-bottom: 60px;
 `
-const ImpressumStyled = styled.div`
-  margin-top: 40px;
+const ImprintStyled = styled.div`
   color: #fc4955;
   font-family: 'Roboto Mono';
-  font-size: 16px;
+  font-size: 14px;
   font-weight: 400;
+  margin-top: 10px;
+  cursor: pointer;
 `
 
 export default class InfoScreen extends Component {
+  state = {
+    hiddenImprint: true,
+    hiddenDataProtection: true,
+  }
+
+  toggleContentImprint = () => {
+    this.setState({
+      hiddenImprint: !this.state.hiddenImprint,
+    })
+  }
+  toggleContentDataProtection = () => {
+    this.setState({
+      hiddenDataProtection: !this.state.hiddenDataProtection,
+    })
+  }
+
+  renderShortOrLongImprint() {
+    return this.state.hiddenImprint ? (
+      <strong>
+        <p onClick={this.toggleContentImprint}>Imprint</p>
+      </strong>
+    ) : (
+      <React.Fragment>
+        <strong>
+          <u onClick={this.toggleContentImprint}>Imprint</u>
+        </strong>
+        <p style={{ color: '#000000' }}>
+          © 2018 Isabella Hof <br />
+          All rights reserved. <br />
+          <br />
+          <strong>Programming, Idea and Conception</strong> <br />
+          Isabella Hof <br />
+          <br />
+          <strong>Responsible</strong> <br />
+          Isabella Hof <br />
+          Barmbeker Str. 9 <br />
+          22303 Hamburg <br />
+          Germany <br />
+          <br />
+          (in German) <br />
+          <strong>Haftungshinweis</strong> <br />
+          Die Inhalte dieser Seite wurde mit größter Sorgfalt erstellt. Für die
+          Richtigkeit, Vollständigkeit und Aktualität der Inhalte kann jedoch
+          keine Gewähr übernohmen werden. Als Diensteanbieter bin ich gemäß § 7
+          Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen
+          Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG bin ich als
+          Diensteanbieter jedoch nicht verpflichtet, übermittelte oder
+          gespeicherte fremde Informationen zu überwachen. Bei Bekanntwerden von
+          entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend
+          entfernen. <br />
+          Trotz sorgfältiger inhaltlicher Kontrolle übernehme ich keine Haftung
+          für die Inhalte externer Links. Für den Inhalt der verlinkten Seiten
+          sind ausschließlich deren Betreiber verantwortlich. <br />
+          <br />
+          <strong>Urheberrecht</strong> <br />
+          Die erstellten Inhalte und Werke dieser Seite unterliegen dem
+          deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung
+          und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes
+          bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw.
+          Erstellers. Soweit die Inhalte auf dieser Seite nicht vom Betreiber
+          erstellt wurden, werden die Urheberrechte Dritter beachtet.
+          Insbesondere werden Inhalte Dritter als solche gekennzeichnet.
+        </p>
+      </React.Fragment>
+    )
+  }
+
+  renderShortOrLongDataProtection() {
+    return this.state.hiddenDataProtection ? (
+      <strong>
+        <p onClick={this.toggleContentDataProtection}>Data Protection</p>
+      </strong>
+    ) : (
+      <React.Fragment>
+        <strong>
+          <u onClick={this.toggleContentDataProtection}>Data Protection</u>
+        </strong>
+        <p style={{ color: '#000000' }}>
+          <strong>your rights and information</strong> <br />
+          If you no longer consent to the storage of your personal data or if
+          import from module; has become incorrect, we will, upon request,
+          delete or block your data or make the necessary corrections (as far as
+          this is possible under applicable law). <br />
+          Upon request, you will receive free of charge information about all
+          personal data that we have stored about you. <br />
+          <br />
+          (in German) <br />
+          Soweit auf den Seiten personenbezogene Daten (beispielsweise Name,
+          Anschrift oder eMail-Adressen) erhoben werden, erfolgt dies, soweit
+          möglich, stets auf freiwilliger Basis. Diese Daten werden ohne Ihre
+          ausdrückliche Zustimmung nicht an Dritte weitergegeben. Wir weisen
+          darauf hin, dass die Datenübertragung im Internet (z.B. bei der
+          Kommunikation per E-Mail) Sicherheitslücken aufweisen kann. Ein
+          lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht
+          möglich. Der Nutzung von im Rahmen der Impressumspflicht
+          veröffentlichten Kontaktdaten durch Dritte zur Übersendung von nicht
+          ausdrücklich angeforderter Werbung und Informationsmaterialien wird
+          hiermit ausdrücklich widersprochen. Die Betreiber der Seiten behalten
+          sich ausdrücklich rechtliche Schritte im Falle der unverlangten
+          Zusendung von Werbeinformationen, etwa durch Spam-Mails, vor. <br />
+        </p>
+      </React.Fragment>
+    )
+  }
+
   render() {
     return (
       <React.Fragment>
-        <Container>
-          <InfoScreenGrid>
+        <InfoScreenContainer>
+          <div>
             <InfoScreenImage>
               <img src={logo_big} alt="Big Logo" />
             </InfoScreenImage>
@@ -60,13 +162,13 @@ export default class InfoScreen extends Component {
               <Link to={'/nids'}>
                 <img src={arrow_right} alt="Arrow points to the right" />
               </Link>
-              <ImpressumStyled>
-                <u>Impressum</u> <br />
-                <u>Datenschutz</u>
-              </ImpressumStyled>
             </InfoScreenRunningText>
-          </InfoScreenGrid>
-        </Container>
+            <ImprintStyled>
+              {this.renderShortOrLongImprint()}
+              {this.renderShortOrLongDataProtection()}
+            </ImprintStyled>
+          </div>
+        </InfoScreenContainer>
       </React.Fragment>
     )
   }
